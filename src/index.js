9ac298fc667f0a1b67be3ed2944c726e1356e447
Elijah Smith <elijahsmith.m@gmail.com>
2022-12-11 16:39:54 -0600

    removed extra comments

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-
-  // whole form element
-  const formContainer = document.querySelector("form");
-  // console.log(formContainer);
-
-  // 
   const form = document.getElementById("create-task-form");
-  console.log(form)
-  // input field
   const inputField = document.getElementById("new-task-description");
-  // console.log(inputField);
+  const outputField = document.getElementById("tasks"); // my todos aka output field / todo list
 
-  // create new task button (added id of submit button)
-  const submitButton = document.getElementById("submit-button");
-  // console.log(submitButton);
-
-  // my todos aka output field / todo list
-  const outputField = document.getElementById("tasks");
-  // console.log(outputField);
-
-  // add Input To Output 
-  // (e) because we're listening for whatever event happens which in this case is "submit"
-    form.addEventListener("submit", (e) => { 
+  form.addEventListener("submit", (e) => {
     e.preventDefault(); // prevent default of going to new url
-      const addedToDo = document.createElement("li"); // create p element
-      addedToDo.innerText = inputField.value; // adds content of input field to p elem
-      const todoList = outputField.appendChild(addedToDo); // adds p element to todo list
-      inputField.value = ""; // reset input field to empty once you submit todo
-    })
+    console.log(e.target.new_task_description);
+    const addedToDo = document.createElement("li"); // create p element
+    addedToDo.innerText = inputField.value; // adds content of input field to p elem
+    outputField.appendChild(addedToDo); // adds p element to todo list
+    inputField.value = ""; // reset input field to empty once you submit todo
+  });
 });
